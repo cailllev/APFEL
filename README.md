225366b9a3ae7ecf1193da59da4b9077cc12173e
cailllev <cailllev@students.zhaw.ch>
2021-01-31 16:06:59 +0100

    finish methods, create first test

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # OWN - File Encription
 ## Description
-Uses RSA Algorithm to encript files. No private key is needed tho, only a password. The password is transformed to the next prime number, this is then used as the secret.
+Uses RSA Algorithm to encript files. No private key is needed tho, only a password. The password is transformed to a bigger (prime) number, this is then used as the decripter (d).
 
 ## Usage
-- create_rsa.py to init numbers for the RSA Algorithm, must be in the same folder as the file to encript / decript
-- encript.py to encript a file with given paramfile
-- decript.py to decript a file with given paramfile and password
+```python3 file_encriptor.py -h```
+- ```-i``` or ```--init``` to create a keyfile with given name
+- ```-k``` or ```--keyfile``` name of the keyfile, contains ```(n,e,diff)```
+- ```-e``` or ```--encript``` to encript a file with given name
+- ```-d``` or ```--decript``` to decript a file with given name
+- ```-v``` or ```--verbose``` to print the decripted file
+- ```-s``` or ```--save``` to save the decripted file
 
 ## TODO
-- is Python really to slow (removing the JVM part)?
-- check security, is this really secure?
-- enhance usage, so that command line params can be used
-- enhance usage, so that the paramfile does not have to be in same dir
+- check security, is this concept secure?
--- file_encriptor.py
+++ file_encriptor.py
@@ -3,12 +3,16 @@ import sys
 import secrets
 import time
 import os.path
+import getpass
 
 from sage.all import ZZ, random_prime, next_prime, inverse_mod, gcd
 
 
-header_keyfile = "======== BEGIN PUBLIC KEYFILE PASSWORD RSA ========\n"
-tail_keyfile = "========= END PUBLIC KEYFILE PASSWORD RSA =========\n"
+BASE = 256  # each char has one byte
+EXTENSION = ".parsa"
+
+HEADER_KEYFILE = "======== BEGIN PUBLIC KEYFILE PASSWORD RSA ========\n"
+TAIL_KEYFILE = "========= END PUBLIC KEYFILE PASSWORD RSA =========\n"
 
 
 def safe_prime(bit_length):
@@ -28,7 +32,7 @@ def safe_prime(bit_length):
             return p
 
 
-def init_keyfile(name):
+def init_keyfile(name, password=None):
 
     if os.path.isfile(name):
         raise FileExistsError("Keyfile " + name + " already exists.")
@@ -45,6 +49,152 @@ def init_keyfile(name):
     n = p*q
     phi = (p-1)*(q-1)
 
+    print(p)
+    print(q)
+
+    del p
+    del q
+
+    if password is None:
+        d = getpass.getpass(prompt="Please enter the password to use for the encription: ")
+    else:
+        d = password
+
+    d = d.encode()
+    d = int.from_bytes(d, "big")
+
+    while True:
+        offset = secrets.randbelow(10**6)
+        d_prime = next_prime(d + offset)
+
+        if gcd(d_prime, phi) == 1:
+            e = inverse_mod(d_prime, n)
+            diff = d_prime - d
+            print("Found valid d and e")
+            break
+
+    print(n)
+    print(e)
+    print(d)
+    print(diff)
+
+    keyfile = open(name, "w")
+    keyfile.write(HEADER_KEYFILE)
+    keyfile.write(n + "\n")
+    keyfile.write(e + "\n")
+    keyfile.write(diff + "\n")
+    keyfile.write(TAIL_KEYFILE)
+    keyfile.close()
+
+
+def encript(filename, keyfile):
+    outfile = filename + EXTENSION
+
+    if os.path.isfile(outfile):
+        raise FileExistsError("Encripted outfile " + outfile + " already exists.")
+
+    k = open(keyfile, "r")
+    n, e, _ = k.readlines()[1:-1]
+    k.close()
+
+    f = open(filename, "rb")
+    data = f.readlines()
+    f.close()
+
+    m = int.from_bytes(data, "big")
+
+    # m^e mod n
+    cipher = pow(m, e, n)
+
+    c = open(filename + EXTENSION, "wb")
+    c.write(cipher)
+    c.close()
+
+    print("Successfully encripted contents of " + filename + " and saved them under " + outfile)
+
+
+def decript(filename, keyfile, show_decripted, save_decripted):
+    outfile = filename[:-len(EXTENSION)]
+
+    if os.path.isfile(outfile):
+        raise FileExistsError("Decripted outfile " + outfile + " already exists.")
+
+    k = open(keyfile, "r")
+    n, _, diff = k.readlines()[1:-1]
+    k.close()
+
+    d = input("Please enter your password you used for the encription: ")
+    d = string_to_number(d)
+
+    f = open(filename, "rb")
+    data = f.readlines()
+    f.close()
+
+    c = int.from_bytes(data, "big")
+
+    # c^d mod n
+    plain = pow(c, d, n)
+    plain = number_to_string(plain)
+
+    print("Successfully decripted contents of " + filename + ".")
+    if show_decripted:
+        print("Result of decription see below.")
+        print("*******************************")
+        print(plain)
+        print("*******************************")
+
+    if save_decripted:
+        m = open(filename + EXTENSION, "wb")
+        m.write(plain)
+        print("Contents saved in " + outfile + ".")
+
+
+def string_to_number(string):
+    if not isinstance(string, str):
+        raise Exception("Only strings allowed: ", string)
+
+    return int(string, base=BASE)
+
+
+def number_to_string(num):
+    if not isinstance(num, int):
+        raise Exception("Only integers allowed: ", num)
+
+    return hex(num)[2:]
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        description='parsa - PAssword RSA.\n'
+                    'Encript and Decript contents of files via RSA algorithm.\n'
+                    'The private key is a password of your choosing.')
+
+    parser.add_argument("-i", "--init",
+                        help="Init a keyfile, name of keyfile.",
+                        type=str)
+
+    parser.add_argument("-k", "--keyfile",
+                        help="Encription and Decription mode, name of keyfile",
+                        type=str)
+
+    parser.add_argument("-e", "--encript",
+                        help="Encription mode, name of file to encript.",
+                        type=str)
+
+    parser.add_argument("-d", "--decript",
+                        help="Decription mode, name of file to decript.",
+                        type=str)
+
+    parser.add_argument("-v", "--verbose",
+                        help="Decription mode, print decripted file.")
+
+    parser.add_argument("-s", "--save",
+                        help="Decription mode, save decripted file.")
+
+    return parser.parse_args(argv)
+
+
+if __name__ == "__main__":
     """
     normal rsa:
     1. choose e
@@ -59,72 +209,9 @@ def init_keyfile(name):
     3. diff = d_prime - d
     4. e is d's mod_inv in phi, if no inv -> go to 2.
     public = (n,e,diff)
-    private = "only a password"
+    private = (n,password)
     """
 
-    d = input("Please enter your password used for the encription: ")
-
-    while True:
-        offset = secrets.randbelow(10**6)
-        d_prime = next_prime(d + offset)
-
-        if gcd(d_prime, phi) == 1:
-            e = inverse_mod(d_prime, n)
-            diff = d_prime - d
-            print("Found valid d and e")
-            break
-
-    print(p)
-    print(q)
-    print(n)
-    print(e)
-    print(d)
-    print(diff)
-
-    keyfile = open(name, "wb")
-    keyfile.write(header_keyfile)
-    keyfile.write(n + b"\n")
-    keyfile.write(e + b"\n")
-    keyfile.write(diff + b"\n")
-    keyfile.write(tail_keyfile)
-
-
-def encript(filename):
-    pass
-
-
-def decript(filename):
-    pass
-
-
-def parse_args(argv):
-    parser = argparse.ArgumentParser(
-        description='Encript and Decript files via RSA algorithm. '
-                    'The private key is a password of your choosing.')
-
-    parser.add_argument("-i", "--init",
-                        help="Init a keyfile, name of keyfile.",
-                        type=str)
-
-    parser.add_argument("-e", "--encript",
-                        help="Encription mode, name of file to encript.",
-                        type=str)
-
-    parser.add_argument("-d", "--decript",
-                        help="Decription mode, name of file to decript.",
-                        type=str)
-
-    parser.add_argument("-v", "--verbose",
-                        help="Decription mode, print decripted file.")
-
-    parser.add_argument("-o", "--out",
-                        help="Decription mode, name to save decripted file with.",
-                        type=str)
-
-    return parser.parse_args(argv)
-
-
-if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
     print(args)
 
@@ -133,14 +220,17 @@ if __name__ == "__main__":
         print("Init keyfile: " + keyfile_name)
         init_keyfile(keyfile_name)
 
-    elif args.encript:
-        file = args.encript
-        print("Encript: " + file)
+    elif args.keyfile:
+        keyfile_name = args.keyfile
 
-        encript(file)
+        if args.encript:
+            file = args.encript
+            print("Encript: " + file)
 
-    elif args.decript:
-        file = args.decript
-        print("Decript: " + args.decript)
+            encript(file, keyfile_name)
 
-        decript(file)
+        elif args.decript:
+            file = args.decript
+            print("Decript: " + args.decript)
+
+            decript(file, keyfile_name, args.verbose, args.save)
